821c163b92243440157f674b5576e20142e1dfbc
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-25 11:18:19 +0100

    Update exp_script.bat

--- exp_script.bat
+++ exp_script.bat
@@ -1,4 +1,4 @@
-export CLASSPATH=./lib/mysql-connector-java-5.1.48-bin.jar
+export CLASSPATH=lib/mysql-connector-java-5.1.48-bin.jar
 queries="$(groovy class_script.gvy)"
 echo "${queries}" | tee logfile.txt
 
